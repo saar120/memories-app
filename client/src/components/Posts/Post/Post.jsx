5de8be3324567e6cb11ac3ad51f9f9f304c39352
saar120 <saar120@gmail.com>
2022-01-24 16:35:02 +0200

    posts setup

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 export default function Post() {
-  return <div>Post</div>;
+  return (
+    <>
+      <div>hello</div>
+      <div>hello</div>
+    </>
+  );
 }
--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@mui/material";
 import Post from "./Post/Post";
 
 export default function Posts() {
@@ -7,12 +8,15 @@ export default function Posts() {
 
   console.log(posts);
 
-  return (
-    <>
-      <h1>posts</h1>
-      <Post />
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid container alignItems="stretch" spacing={3}>
+      {posts.map((post) => (
+        <Grid item key={post._id} xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
