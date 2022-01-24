39cb9a5c7369a2c00c6eae9724cc09df0e75e735
saar120 <saar120@gmail.com>
2022-01-24 18:29:47 +0200

    post

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,14 +1,43 @@
 import React from "react";
+import moment from "moment";
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
-    <>
-      <div>hello</div>
-      <div>hello</div>
-    </>
+    <Card>
+      <CardMedia component="img" src={post.selectedFile} title={post.title} />
+      <div>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div>
+        <Button style={{ color: "white" }} size="small">
+          <MoreHorizIcon style={{ color: "black" }} />
+        </Button>
+      </div>
+      <div>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag}`)}
+        </Typography>
+      </div>
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          <ThumbUpIcon />
+          Like {post.likeCount}
+        </Button>
+        <Button size="small" color="primary">
+          <DeleteIcon />
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
