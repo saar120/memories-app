72dfecd47375e1a820ea6764298a151879dfb617
saar120 <saar120@gmail.com>
2022-01-25 13:41:43 +0200

    add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
-import { Container, Grow, Grid } from "@mui/material";
-import { AppBarStyled, HeaderStyled } from "./styles";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Container } from "@mui/material";
+import { Navbar } from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 export default function App() {
-  const dispatch = useDispatch();
-  const [currentId, setCurrentId] = useState(null);
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
-    <Container maxWidth="lg">
-      <AppBarStyled position="static" color="inherit">
-        <HeaderStyled variant="h2" align="center">
-          Blogs
-        </HeaderStyled>
-      </AppBarStyled>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <Router>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/auth" exact element={<Auth />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { getPosts } from "../../actions/posts";
+import { Container, Grow, Grid } from "@mui/material";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+
+export default function Home() {
+  const dispatch = useDispatch();
+  const [currentId, setCurrentId] = useState(null);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+  return (
+    <Grow in>
+      <Container>
+        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+}
