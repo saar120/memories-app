83b4cc0910cc1bf7374319ceb7d062907fb1bfbd
saar120 <saar120@gmail.com>
2022-01-25 23:27:49 +0200

    changed error handaling

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -10,12 +10,16 @@ export const signIn = async (req, res) => {
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
-      throw new Error({ message: "User not found", code: 404 });
+      const error = new Error("User not found");
+      error.code = 404;
+      throw error;
     }
     const isPasswordValid = await bcrypt.compare(password, existingUser.password);
 
     if (!isPasswordValid) {
-      throw new Error({ message: "Invalid credentials", code: 403 });
+      const error = new Error("Invalid credentials");
+      error.code = 400;
+      throw error;
     }
 
     const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, jwtSecret, {
@@ -33,10 +37,14 @@ export const signUp = async (req, res) => {
     const { email, password, confirmPassword, firstName, lastName } = req.body;
     const existingUser = await User.findOne({ email: email });
     if (existingUser) {
-      throw new Error({ message: "User already exists", code: 400 });
+      const error = new Error("User already exists");
+      error.code = 400;
+      throw error;
     }
     if (password !== confirmPassword) {
-      throw new Error({ message: "Passwords dont match", code: 400 });
+      const error = new Error("Passwords dont match");
+      error.code = 400;
+      throw error;
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -45,8 +53,9 @@ export const signUp = async (req, res) => {
 
     const token = jwt.sign({ email: result.email, id: result._id }, jwtSecret, { expiresIn: "1h" });
 
-    res.status(200).json(result, token);
+    res.status(200).json({ result, token });
   } catch (err) {
+    console.log(err);
     res.status(err.code || 500).json({ message: err.message || "Something went wrong" });
   }
 };
